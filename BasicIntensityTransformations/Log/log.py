f11047c3e9178a7f843fe9d759419b7dd5c0b1ad
Tashrif Ahmad <tashrif@revesoft.com>
2022-01-17 05:20:47 +0600

    added Log Transformation

--- BasicIntensityTransformations/Log/log.py
+++ BasicIntensityTransformations/Log/log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 17 02:42:48 2022
+Created on Mon Jan 17 04:51:25 2022
 
 @author: Taif
 """
@@ -8,15 +8,19 @@ Created on Mon Jan 17 02:42:48 2022
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+def normalize(x, min_, max_, tarMin, tarMax):
+    assert max_ - min_ != 0  
+    fx = ((tarMax - tarMin) * (x - min_) / (max_ - min_)) + tarMin
+    return fx
 
 def readImage(dirOfFile):
     img = cv2.imread(dirOfFile)
-    
     return img
 
 def toGray(img):
     grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
     return grayImage
 
 def display(img, title = 'Image', cmap_type = 'gray'):
@@ -32,25 +36,24 @@ def wait():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def negativeOf(img):
+def logOf(img, c = 1):
     n = img.shape[0]
     m = img.shape[1]
     output = np.zeros(img.shape)
 
     for i in range(n):
         for j in range(m):
-            output[i][j] = 255 - img[i][j]
+            output[i][j] = c * math.log(1 + img[i][j])
+            output[i][j] = normalize(output[i][j], 0, c * math.log(256), 0, 255)
     
     return output
 
 def main():
     input_image = toGray(readImage("input.jpg"))
     display(input_image, "Input Image")
-    output = negativeOf(input_image)
+    output = logOf(input_image, 1) 
     display(output, "Output Image")
     saveImage("output.jpg", output)
     wait()
     
 main()
-    
-    

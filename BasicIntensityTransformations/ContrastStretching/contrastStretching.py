0565d82df5cfc757dbf15c18b685ddcc1e9eab43
Tashrif Ahmad <tashrif@revesoft.com>
2022-01-18 06:26:44 +0600

    updated for color image

--- BasicIntensityTransformations/ContrastStretching/contrastStretching.py
+++ BasicIntensityTransformations/ContrastStretching/contrastStretching.py
@@ -23,8 +23,9 @@ def display(img, title = 'Image', cmap_type = 'gray'):
     plt.axis('off')
     plt.show()
 
-def showHistogramOf(img):
+def showHistogramOf(img, title = "histogram"):
     plt.hist(img.ravel(), 256, [0, 256])
+    plt.title(title)
     plt.show()
     
 def saveImage(dir_, img):
@@ -35,25 +36,27 @@ def wait():
     cv2.destroyAllWindows()
 
 def contrastStretch(img, s1, s2):
-    n = img.shape[0]
-    m = img.shape[1]
+    n = img.shape[1]
+    m = img.shape[2]
     output = img.copy()
-    r2 = img.max()
-    r1 = img.min()
+    
+    for c in range(img.shape[0]):
+        r2 = img[c].max()
+        r1 = img[c].min()
 
-    for i in range(n):
-        for j in range(m):
-            output.itemset((i, j), (s2 - s1) * (img.item(i, j) - r1) / (r2 - r1) + s1)
+        for i in range(n):
+            for j in range(m):
+                output[c].itemset((i, j), (s2 - s1) * (img[c].item(i, j) - r1) / (r2 - r1) + s1)
     
     return output
 
 def main():
-    input_ = toGray(readImage("input.jpg"))
-    display(input_, "Input (Gray)")
-    showHistogramOf(input_)
-    output_ = contrastStretch(input_, 50, 55)
+    input_ = readImage("input.jpg")
+    display(input_, "Input image")
+    showHistogramOf(input_, "Histogram of input image")
+    output_ = contrastStretch(input_, 0, 255)
     display(output_, "Output image")
-    showHistogramOf(output_)
+    showHistogramOf(output_, "Histogram of output image")
     saveImage("output.jpg", output_)
     
     wait()
